c43d200ada7b064692ac9e586cd807bc47165070
Shan <shantanusinghshakya25@gmail.com>
2025-06-13 01:02:14 +0530

    we can now get list of all posts, as i have implemented the feature, getblogpost

---
+++ controller/getBlogPost.js
@@ -0,0 +1,15 @@
+const BlogPost = require("../model/blogpost.schema");
+
+async function getBlogPost(req , res){
+    try{
+        const posts = await BlogPost.find();
+        if(!posts)
+            return res.status(200).send("No blogs right now")
+        return res.status(200).json({posts});
+    } catch(e){
+        console.error(e.message);
+        return res.status(400).json({status: "An error occured. " , error: e.message});
+    };
+};
+
+module.exports = deleteBlogPost;
--- routes/blogposts.route.js
+++ routes/blogposts.route.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const createBlogPost = require("./../controller/postblog");  // controller fucntion to post a blog to db.
 const deleteBlogPost = require("../controller/deletepost");
 const updateBlogPost = require("../controller/updatepost");
+const getBlogPost = require("../controller/getBlogPost");
+
 
 //routes
+router.get("/" , getBlogPost);
 router.post("/" , createBlogPost);
 router.delete("/:id" , deleteBlogPost);
 router.put("/:id" , updateBlogPost);

8247db813efeb8af268a14743e4bad9917926d93
Shan <shantanusinghshakya25@gmail.com>
2025-06-13 00:55:31 +0530

    Updated routes file , now update and delete blog feature are implemented in the file

--- routes/blogposts.route.js
+++ routes/blogposts.route.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 //controller fucntions
 const createBlogPost = require("./../controller/postblog");  // controller fucntion to post a blog to db.
+const deleteBlogPost = require("../controller/deletepost");
+const updateBlogPost = require("../controller/updatepost");
 
 //routes
 router.post("/" , createBlogPost);
+router.delete("/:id" , deleteBlogPost);
+router.put("/:id" , updateBlogPost);
 
 module.exports = router;

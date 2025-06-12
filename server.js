709a52d3d15705e52c6b7d4279d295fec1754608
Shan <shantanusinghshakya25@gmail.com>
2025-06-13 01:04:34 +0530

    updating server.js. implementing routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 3000
 const db_setup = require("./db/setup");
-
+const bp_router = require("./routes/blogposts.route")
 // setting up the database
 db_setup();
 
@@ -14,6 +14,9 @@ app.use(express.static(path.join(__dirname , "public")));
 // parsing the body, json and url-encoded data.
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
+
+// routing
+app.use("/blog" , bp_router);
 
 app.listen(PORT , function(){
     console.log("The server is running on port " , PORT , `and the direct link is http://localhost:${PORT}`);
